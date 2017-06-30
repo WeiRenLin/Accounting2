5f153b1f1cb25870391f38ad9151f9a4860ed5bf
林威任 <s3211t@hotmail.com.tw>
2017-06-30 16:41:16 +0800

    初始化

---
+++ AccountPractice/AccountPractice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AccountPractice.MvcApplication" Language="C#" %>
---
+++ AccountPractice/AccountPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountPractice.Startup))]
+namespace AccountPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
